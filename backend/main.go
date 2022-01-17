684b68e1008960ad0352b646d10b3e560f0a8124
lunarwhite <dwcn22@outlook.com>
2022-01-17 19:49:31 +0800

    split out Websocket code into pkg

--- backend/main.go
+++ backend/main.go
@@ -2,63 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github.com/lunarwhite/chat-app/pkg/websocket"
 )
 
-// define an Upgrader.
-// upgrade the connection from a standard HTTP endpoint
-// to a long-lasting WebSocket connection.
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-
-	// check the origin of our connection
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-// define a reader.
-// it will listen for new messages being sent to WebSocket endpoint.
-func reader(conn *websocket.Conn) {
-	for {
-		// read in a message.
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		// print out that message for clarity.
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
 // define a WebSocket endpoint.
+// upgrade this connection to a WebSocket connection.
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
-	// upgrade this connection to a WebSocket connection.
-	ws, err := upgrader.Upgrade(w, r, nil)
+	ws, err := websocket.Upgrade(w, r)
 	if err != nil {
-		log.Println(err)
+		fmt.Fprintf(w, "%+V\n", err)
 	}
 
-	// listen indefinitely for new messages coming through on the WebSocket connection.
-	reader(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
+// mape our `/ws` endpoint to the `serveWs` function.
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Simple Server")
-	})
-	// mape our `/ws` endpoint to the `serveWs` function.
 	http.HandleFunc("/ws", serveWs)
 }
 
